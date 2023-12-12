7bcfc56b89daff991cdd9cfc8a2473a56a23f617
davexxx <davexxx@163.com>
2023-12-12 20:18:40 +0800

    fix issue

--- sum4all.py
+++ sum4all.py
@@ -164,6 +164,10 @@ class sum4all(Plugin):
 
         if e_context['context'].type == ContextType.TEXT:
             if content.startswith(self.image_sum_trigger) and self.image_sum:
+                if user_id not in self.params_cache:
+                    self.params_cache[user_id] = {}
+                    logger.info('Added new user to params_cache.')
+
                 # Call new function to handle search operation
                 pattern = self.image_sum_trigger + r"\s(.+)"
                 match = re.match(pattern, content)
@@ -180,6 +184,10 @@ class sum4all(Plugin):
                 e_context.action = EventAction.BREAK_PASS
 
             elif content.startswith(self.image_sum_batch_trigger) and self.image_sum:
+                if user_id not in self.params_cache:
+                    self.params_cache[user_id] = {}
+                    logger.info('Added new user to params_cache.')
+
                 # Call new function to handle search operation
                 self.params_cache[user_id]['image_sum_quota'] = 5
                 self.params_cache[user_id]['image_prompt'] = self.image_prompt
@@ -188,6 +196,10 @@ class sum4all(Plugin):
                 e_context.action = EventAction.BREAK_PASS
 
             elif content.startswith(self.close_image_sum_trigger) and self.image_sum:
+                if user_id not in self.params_cache:
+                    self.params_cache[user_id] = {}
+                    logger.info('Added new user to params_cache.')
+                    
                 # Call new function to handle search operation
                 self.params_cache[user_id]['image_sum_quota'] = 0
                 reply = Reply(type=ReplyType.TEXT, content="已关闭识图模式，您的图片不再继续进行识别。")
